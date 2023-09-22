f34205dfbbc7fa3d2aeb9dbe4fd446ec0ae4697d
zulhusnihariz <zulhusnihariz97@gmail.com>
2023-09-23 01:03:47 +0800

    add tanstack/react-query

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,23 @@ import ReactDOM from 'react-dom/client'
 import App from 'App'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchInterval: 0,
+      refetchOnWindowFocus: false,
+      retry: false,
+    },
+  },
+})
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
 )
